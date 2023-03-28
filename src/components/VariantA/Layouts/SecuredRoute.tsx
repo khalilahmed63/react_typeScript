8927a87114d04d453dd370e9dcb1cafd83cd87e1
khalilahmed63 <panhwarkhalilahmed@gmail.com>
2023-03-28 16:49:37 +0500

    fixed

--- src/components/VariantA/Layouts/SecuredRoute.tsx
+++ src/components/VariantA/Layouts/SecuredRoute.tsx
@@ -5,14 +5,13 @@ import { useNavigate } from "react-router-dom";
 export default function SecuredRoute(props: any) {
   const navigate = useNavigate();
   const { setLoginModal } = useStoreActions((actions: any) => actions.global);
-  const { loginModal } = useStoreState((state: any) => state.global);
   useEffect(() => {
     // checks if the user is authenticated
     if (!localStorage.getItem("token")) {
       setLoginModal(true);
       navigate("/");
     }
-  }, [loginModal]);
+  }, []);
 
   return <div>{props.children}</div>;
 }
